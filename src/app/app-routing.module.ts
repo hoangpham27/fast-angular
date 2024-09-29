7af859639f0748ebd6be37238f0686b54fc4cf99
hoangpham27 <pvh2772002@gmail.com>
2024-09-29 14:29:57 +0700

    Lazy Loading Feature Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,17 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'home', component: HomeComponent },
   { path: '', component: HomeComponent },
-  { path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page];
+  // { path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page];
+  {
+    path: 'customers',
+    loadChildren: () =>
+      import('./customers/customers.module').then((m) => m.CustomersModule),
+  },
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./orders/orders.module').then((m) => m.OrdersModule),
+  },
 ];
 
 @NgModule({
